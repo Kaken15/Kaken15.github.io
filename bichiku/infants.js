b62073489b3454ef14265abd8c6630dc9c28c4a3
Kaken15 <152462729+Kaken15@users.noreply.github.com>
2024-01-12 05:25:21 +0900

    infants.js

--- bichiku/infants.js
+++ bichiku/infants.js
@@ -2,13 +2,15 @@
 const checkboxes=document.getElementsByClassName("checks");
 //結果表示のボタンを取得して、変数に格納
 const btn=document.getElementById("btn");
-//
+//resultsというidを取得して、変数に格納
 const resultsDiv=document.getElementById("results");
+//未チェックのチェックリストを取得
 const getUncheckedValues=()=>{
+//チェックリストの数分ループして、uncheckedValuesという配列に追加
     var uncheckedValues = [];
     for(let i=0;i<checkboxes.length;i++){
 
-
+//未チェックのチェックリストを確認し、アラートメッセージかResultButton関数を呼び出す
         if(!checkboxes[i].checked){
             uncheckedValues.push(checkboxes[i].value);
         }
@@ -21,6 +23,7 @@ const getUncheckedValues=()=>{
         alert("Allcheckboxes are checked.");
     }
 };
+
 btn.addEventListener("click",getUncheckedValues,false);
 
 
